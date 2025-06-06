e6386518f018487b2b87a6aa55bb1ed71eb24fb4
ryan Fidelis <ryanmumu1@gmail.com>
2025-06-06 12:18:38 -0300

    atualização

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const imagens = document.querySelectorAll(".imgC");
+const imagens = document.querySelectorAll(".img-prod");
 const slide = document.querySelectorAll(".slide")
+const btnPrev = document.getElementById("prev")
+const btnNext = document.getElementById("next")
 
 let timer;
 let active = 0
 let total = imagens.length
 
 const update = (direction)=>{
-    document.querySelector('.imgC.ativa').classList.remove('ativa')
+    document.querySelector('.img-prod.ativa').classList.remove('ativa')
     document.querySelector('.slide.ativar').classList.remove('ativar')
 
     if(direction > 0){
@@ -28,8 +30,11 @@ const update = (direction)=>{
 clearTimeout(timer)
 timer = setInterval(() => {
     update(1)
-}, 6000);
+}, 8000);
 
-// imagens.addEventListener('click', ()=>{
-//     update(1)
-// })
+btnNext.addEventListener('click', ()=>{
+    update(1)
+})
+btnPrev.addEventListener('click',()=>{
+    update(-1)
+})
